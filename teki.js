e7a976e7dd01b37151bade34dd7f09ce6a0add02
aragosta1226 <djaragosta1226@gmail.com>
2021-12-19 17:08:33 +0900

    firstcommit

---
+++ teki.js
@@ -0,0 +1,133 @@
+//
+//teki.js 敵関連
+//
+
+// 敵弾クラス
+class Teta extends CharaBase {
+    
+    constructor(sn, x, y, vx, vy) {
+        super(sn, x, y, vx, vy);
+        this.r = 4;
+    }
+    
+    update(){
+        super.update();
+            if(!jiki.damage && checkHit(this.x, this.y, this.r, jiki.x, jiki.y, jiki.r)) {
+
+            this.kill = true;
+            jiki.damage = 10;
+            jiki.muteki = 60;
+            }
+            this.sn = 14 + ((this.count >> 3) & 1);
+    }
+}
+
+// 敵クラス
+class Teki extends CharaBase {
+    constructor(tnum, x, y, vx, vy) {
+        super(0, x, y, vx, vy);
+        this.flag = false;
+        // this.w = 20;
+        // this.h = 20;
+        this.r = 10;
+        this.tnum = tnum;
+    }
+
+    update() {
+        //共通のアップデート
+        super.update();
+        // tekiMove();
+
+        //当たり判定
+        if(!this.flag) {
+            if(jiki.x > this.x && this.vx < 120) this.vx += 4;
+            else if(jiki.x < this.x && this.vx > -120) this.vx -= 4;
+        } else {
+            if(jiki.x < this.x && this.vx < 400) this.vx += 30;
+            else if(jiki.x > this.x && this.vx > -400) this.vx -= 30;
+        }
+        if(Math.abs(jiki.y-this.y) < (100 << 8) && !this.flag) {
+            this.flag = true;
+
+            let an, dx, dy;
+            an = Math.atan2(jiki.y - this.y, jiki.x - this.x);
+
+            an += rand(-3, 3) * Math.PI/180; 
+            
+            dx = Math.cos(an) * 1000;
+            dy = Math.sin(an) * 1000;
+
+            teta.push(new Teta(15, this.x, this.y, dx, dy));
+        }
+        if(!jiki.damage && checkHit(this.x, this.y, this.r, jiki.x, jiki.y, jiki.r)) {
+            this.kill = true;
+            jiki.damage = 10;
+            jiki.muteki = 60;
+        }
+        if(this.flag && this.vy > -800) this.vy -= 30;
+    }
+
+    draw() {
+        super.draw();
+    }
+}
+
+//ピンクのひよこの移動パターン
+// function tekiMove01() {
+//     if(!this.flag) {
+//         if(jiki.x > this.x && this.vx < 120) this.vx += 4;
+//         else if(jiki.x < this.x && this.vx > -120) this.vx -= 4;
+//     } else {
+//         if(jiki.x < this.x && this.vx < 400) this.vx += 30;
+//         else if(jiki.x > this.x && this.vx > -400) this.vx -= 30;
+//     }
+//     if(Math.abs(jiki.y-this.y) < (100 << 8) && !this.flag) {
+//         this.flag = true;
+
+//         let an, dx, dy;
+//         an = Math.atan2(jiki.y - this.y, jiki.x - this.x);
+
+//         an += rand(-3, 3) * Math.PI/180; 
+        
+//         dx = Math.cos(an) * 1000;
+//         dy = Math.sin(an) * 1000;
+
+//         teta.push(new Teta(15, this.x, this.y, dx, dy));
+//     }
+//     if(!jiki.damage && checkHit(this.x, this.y, this.r, jiki.x, jiki.y, jiki.r)) {
+//         this.kill = true;
+//         jiki.damage = 10;
+//         jiki.muteki = 60;
+//     }
+//     if(this.flag && this.vy > -800) this.vy -= 30;
+// }
+
+// //黄色のひよこの移動パターン
+// function tekiMove02() {
+//     if(!this.flag) {
+//         if(jiki.x > this.x && this.vx < 120) this.vx += 4;
+//         else if(jiki.x < this.x && this.vx > -120) this.vx -= 4;
+//     } else {
+//         if(jiki.x < this.x && this.vx < 400) this.vx += 30;
+//         else if(jiki.x > this.x && this.vx > -400) this.vx -= 30;
+//     }
+//     if(Math.abs(jiki.y-this.y) < (100 << 8) && !this.flag) {
+//         this.flag = true;
+
+//         let an, dx, dy;
+//         an = Math.atan2(jiki.y - this.y, jiki.x - this.x);
+
+//         an += rand(-3, 3) * Math.PI/180; 
+        
+//         dx = Math.cos(an) * 1000;
+//         dy = Math.sin(an) * 1000;
+
+//         teta.push(new Teta(15, this.x, this.y, dx, dy));
+//     }
+//     if(!jiki.damage && checkHit(this.x, this.y, this.r, jiki.x, jiki.y, jiki.r)) {
+//         this.kill = true;
+//         jiki.damage = 10;
+//         jiki.muteki = 60;
+//     }
+//     if(this.flag && this.vy > -800) this.vy -= 30;
+// }
