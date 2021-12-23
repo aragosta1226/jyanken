f16b3ee1beae3413f753de428aa4cafe2212dcb4
aragosta1226 <djaragosta1226@gmail.com>
2021-12-23 23:54:46 +0900

    コード変更

---
+++ omikuji.js
@@ -0,0 +1,10 @@
+const randomNumber1 = Math.random();
+
+if (`0 === 0`) {
+    // 条件式を満たす場合の処理
+    window.location.href = "{index2.html}";
+  } else {
+    // 条件式を満たさない場合の処理
+    window.location.href = "{nansore.jpg}";
+  }
+  
